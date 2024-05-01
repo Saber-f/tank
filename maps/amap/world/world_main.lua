882d7f471047b7facad3f400b6a452191734dcfd
fdy <735974175@qq.com>
2024-05-02 02:46:08 +0800

    不产生虫子

--- maps/amap/world/world_main.lua
+++ maps/amap/world/world_main.lua
@@ -72,52 +72,53 @@ local function build_base(surface,maxs,event,position)
 
   if x>=80 and x<=400 then
     if y>-300 and y<300 then
-      surface.set_tiles({{name = "lab-dark-2", position = position}})
+      surface.set_tiles({{name = "sand-1", position = position}})
       return
     end
   end
 
-  if x>=-128 and x<80 then
-    if y>=-64 and y<64 then
-      surface.set_tiles({{name = "grass-1", position = position}})
-      return
-    end
-  end
+  -- if x>=-128 and x<80 then
+  --   if y>=-64 and y<64 then
+  --     surface.set_tiles({{name = "grass-1", position = position}})
+  --     return
+  --   end
+  -- end
 
   
-  surface.set_tiles({{name = "water", position = position}})
+  surface.set_tiles({{name = "grass-1", position = position}})
   
-  x = math.abs(x)
-  y = math.abs(y)
-  if x >= 192 or y >= 128 then
-    if math.fmod(x,8) < 1 and math.fmod(y,8) < 1 then
-      local n = math.abs(position.x + 64)
-      if (y > n) then n = y end
-      n = (n - 128 + 8) / 8
-      local m = 2
-      if (n > 10) then
-        m = 2^(n-9)
-        n = 10
-      end
-      local name = mowang[math.random(1,11)]..n
-      local x2 = position.x - 0.5
-      if position.x < 0 then
-        x2 = position.x + 0.5
-      end
-      local y2 = position.y 
-      if position.y < 0 then
-        y2 = position.y + 0.5
-      end
+  -- surface.set_tiles({{name = "water", position = position}})
+  
+  -- x = math.abs(x)
+  -- y = math.abs(y)
+  -- if x >= 192 or y >= 128 then
+  --   if math.fmod(x,8) < 1 and math.fmod(y,8) < 1 then
+  --     local n = math.abs(position.x + 64)
+  --     if (y > n) then n = y end
+  --     n = (n - 128 + 8) / 8
+  --     local m = 2
+  --     if (n > 10) then
+  --       m = 2^(n-9)
+  --       n = 10
+  --     end
+  --     local name = mowang[math.random(1,11)]..n
+  --     local x2 = position.x - 0.5
+  --     if position.x < 0 then
+  --       x2 = position.x + 0.5
+  --     end
+  --     local y2 = position.y 
+  --     if position.y < 0 then
+  --       y2 = position.y + 0.5
+  --     end
 
 
-      local biter = surface.create_entity({name = name, position = {x2, y2}, force = 'enemy'})
-      if x == 224 and y == 0 then
-        game.print("x:"..x2.." y:"..y2.." n:"..n.." m:"..m)
-      end
-      BiterHealthBooster.add_boss_unit(biter, m, 0.55)
-    end
-  else
-  end
+  --     local biter = surface.create_entity({name = name, position = {x2, y2}, force = 'enemy'})
+  --     if x == 224 and y == 0 then
+  --       game.print("x:"..x2.." y:"..y2.." n:"..n.." m:"..m)
+  --     end
+  --     BiterHealthBooster.add_boss_unit(biter, m, 0.55)
+  --   end
+  -- end
 
 
   
